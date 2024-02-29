72be57d2ad87697066830ceda63fbf1a47fae02e
Caio Eduardo <84157848+caioed77@users.noreply.github.com>
2024-02-28 21:12:41 -0300

    melhoria no relatorio de caixa

--- api-caixa/src/main/java/com/apiCaixaFinanceiro/apicaixa/application/Transacoes/RelatorioTransacoesService.java
+++ api-caixa/src/main/java/com/apiCaixaFinanceiro/apicaixa/application/Transacoes/RelatorioTransacoesService.java
@@ -6,6 +6,7 @@ import com.apiCaixaFinanceiro.apicaixa.domain.dto.DadosTransacaoDTO;
 import com.apiCaixaFinanceiro.apicaixa.domain.dto.GerarRelatorioDTO;
 import com.apiCaixaFinanceiro.apicaixa.domain.dto.RetornaSaldoDTO;
 import com.itextpdf.text.*;
+import com.itextpdf.text.pdf.PdfPTable;
 import com.itextpdf.text.pdf.PdfWriter;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.jdbc.core.JdbcTemplate;
@@ -15,7 +16,9 @@ import org.springframework.stereotype.Service;
 import java.io.ByteArrayOutputStream;
 import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
+import java.text.SimpleDateFormat;
 import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
 import java.util.*;
 import java.util.List;
 
@@ -47,37 +50,80 @@ public class RelatorioTransacoesService {
             try {
                   PdfWriter.getInstance(document, new FileOutputStream(caminhoRelatorio));
                   document.open();
+                  document.add(new Paragraph("Relatorio caixa", new Font(Font.FontFamily.TIMES_ROMAN, 16, Font.BOLD)));
 
-                  document.add(new Paragraph("Saldo do Caixa:"));
-                  var paragraphSaldoCaixa = new Paragraph(dadosCaixa.getSaldo().toString());
-                  document.add(paragraphSaldoCaixa);
+                  document.add(new Paragraph("\n"));
+                  document.add(new Paragraph("Dados Transações:", new Font(Font.FontFamily.TIMES_ROMAN, 12, Font.BOLD)));
+                  document.add(new Paragraph("\n"));
+
+                  PdfPTable table = new PdfPTable(3);
+                  table.addCell("Tipo");
+                  table.addCell("Data");
+                  table.addCell("Saldo");
+
+                  for (var row : dadosTransacoes) {
+
+                        var paragraphTipo = getTipo(row);
+                        var dataTransactionFormat = row.dataTransacao().format(DateTimeFormatter.ofPattern("dd/MM/yyyy"));
+
+                        table.addCell(paragraphTipo);
+                        table.addCell(new Paragraph("\n"));
+
+                        var paragraphSaldo = new Paragraph(row.valorTransferencia().toString());
+                        table.addCell(paragraphSaldo);
+                        table.addCell(new Paragraph("\n"));
+
+                        var paragraphData = new Paragraph(dataTransactionFormat);
+                        table.addCell(paragraphData);
+                        table.addCell(new Paragraph("\n"));
+                  }
+                  document.add(table);
+
+                  document.add(new Paragraph("Saldo Total", new Font(Font.FontFamily.TIMES_ROMAN, 12, Font.BOLD)));
+                  var paragraphSaldoTotal = new Paragraph(String.valueOf(saldoTotalTransacoes.get(0).Saldo()));
+                  document.add(paragraphSaldoTotal);
 
                   document.add(new Paragraph("\n"));
 
-                  document.add(new Paragraph("Dados Transações:"));
-                  for (var row : dadosTransacoes) {
-                        var paragraphTipo = new Paragraph(row.tipoTransacao());
-                        document.add(paragraphTipo);
-                        document.add(new Paragraph("\n"));
-
-                        var paragraphData = new Paragraph(row.dataTransacao().toString());
-                        document.add(paragraphData);
-                        document.add(new Paragraph("\n"));
-
-                        var paragraphSaldo = new Paragraph(row.valorTransferencia().toString());
-                        document.add(paragraphSaldo);
-                        document.add(new Paragraph("\n"));
-                  }
-                  document.add(new Paragraph("Saldo Total"));
-                  var paragraphSaldoTotal = new Paragraph(String.valueOf(saldoTotalTransacoes.get(0).Saldo()));
-                  document.add(paragraphSaldoTotal);
+                  document.add(new Paragraph("Saldo do Caixa:", new Font(Font.FontFamily.TIMES_ROMAN, 12, Font.BOLD)));
+                  var paragraphSaldoCaixa = new Paragraph(dadosCaixa.getSaldo().toString());
+                  document.add(paragraphSaldoCaixa);
             } catch (FileNotFoundException e) {
                   throw new RuntimeException(e);
             } finally {
-                  document.close();
+                  if (document.isOpen()) {
+                        document.close();
+                  }
             }
 
             return outputStream;
+      }
+
+      private static Paragraph getTipo(DadosTransacaoDTO row) {
+            String tipo = "";
+
+            switch (row.tipoTransacao()){
+                  case "E" -> {
+                        tipo = "Entrada";
+                  }
+                  case "S" -> {
+                        tipo = "Saida";
+                  }
+                  case "T" -> {
+                        tipo = "Transferencia caixa";
+                  }
+                  case "R" -> {
+                        tipo = "Transferencia reserva";
+                  }
+                  case "X" -> {
+                        tipo = "Sangria";
+                  }
+                default -> {
+                    break;
+                }
+            }
+
+            return new Paragraph(tipo);
       }
 
       private List<RetornaSaldoDTO> obtemSaldoTransacao(LocalDate dataInicial, LocalDate dataFinal, String tipoTransacao) {
@@ -105,7 +151,6 @@ public class RelatorioTransacoesService {
 
             return jdbcTemplate.query(sqlTransacao.toString(), listaParametros.toArray(), transacaoSaldoRowMapper);
       }
-
 
       private List<DadosTransacaoDTO> obtemDadosTransacoes(LocalDate dataInicial, LocalDate dataFinal, String tipoTransacao) {
             var sqlTransacao = new StringBuilder();
