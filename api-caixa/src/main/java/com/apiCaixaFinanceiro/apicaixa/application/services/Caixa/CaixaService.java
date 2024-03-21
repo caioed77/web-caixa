e5f217618239c4b36dd633195bebf531e2c1947f
Caio Eduardo <84157848+caioed77@users.noreply.github.com>
2024-03-21 20:29:50 -0300

    correcoes

--- api-caixa/src/main/java/com/apiCaixaFinanceiro/apicaixa/application/services/Caixa/CaixaService.java
+++ api-caixa/src/main/java/com/apiCaixaFinanceiro/apicaixa/application/services/Caixa/CaixaService.java
@@ -2,6 +2,7 @@ package com.apiCaixaFinanceiro.apicaixa.application.services.Caixa;
 
 import com.apiCaixaFinanceiro.apicaixa.domain.entities.CaixaEntity;
 import com.apiCaixaFinanceiro.apicaixa.domain.repositories.CaixaRepository;
+import com.apiCaixaFinanceiro.apicaixa.infra.exceptions.ResouceNotFoundException;
 import com.apiCaixaFinanceiro.apicaixa.infra.repositoriesJpa.CaixaRepositoryJpa;
 import org.springframework.stereotype.Service;
 
@@ -9,7 +10,6 @@ import java.util.Optional;
 
 @Service
 public class CaixaService {
-
       private final CaixaRepository caixaRepository;
 
       public CaixaService(CaixaRepository caixaRepository) {
@@ -17,7 +17,7 @@ public class CaixaService {
       }
 
       public Optional<CaixaEntity> retornaSaldoCaixa(Long id) {
-            return Optional.of(caixaRepository.findById(id).get());
+            return Optional.of(caixaRepository.findById(id)
+                    .orElseThrow(() -> new ResouceNotFoundException(id)));
       }
-
 }
